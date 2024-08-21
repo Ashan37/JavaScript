de36059561dd798bd3c5ab0fe4ac5634ed532866
Ashan Ekanayake <ashanekanayakeat@gmail.com>
2024-08-21 12:22:42 +0530

    arithmetic operations

---
+++ Arithmetic Options/index.js
@@ -0,0 +1,23 @@
+let students=20;
+console.log(students);
+
+let extrastudents=students+20;
+console.log(extrastudents);
+
+extrastudents+=5;
+console.log(extrastudents);
+
+/*
+operator precedence
+
+1)parenthesis
+2)exponents
+3)multipication & division
+4)addition & substraction
+
+*/
+
+let result=10+20*(6);
+console.log(result);
+
+
