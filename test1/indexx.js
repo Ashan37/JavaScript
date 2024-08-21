4fa7615e413505a394d109bbbe0b9314896eefaa
Ashan Ekanayake <ashanekanayakeat@gmail.com>
2024-08-21 11:31:29 +0530

    basic javascript

---
+++ test1/indexx.js
@@ -0,0 +1,12 @@
+console.log("I like pizza!");
+
+
+
+let age;
+age=21;
+
+console.log("My age is "+age);
+
+age=age+5;
+console.log("After 5 years, my age is "+age);
+document.getElementById("q1").innerHTML="Hello";
