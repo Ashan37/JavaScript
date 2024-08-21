826c086cf18d3180b2a61dd2a5d1ba772d7cada4
Ashan Ekanayake <ashanekanayakeat@gmail.com>
2024-08-21 12:05:07 +0530

    first update

--- test1/indexx.js
+++ test1/indexx.js
@@ -1,12 +1,14 @@
 console.log("I like pizza!");
 
 
+let firstname= "John";
+let age=21;
 
-let age;
-age=21;
+console.log("Hello "+firstname)
+console.log("Your age is "+age);
 
-console.log("My age is "+age);
+document.getElementById("q1").innerHTML="Hello "+firstname+".";
+document.getElementById("q2").innerHTML=firstname+" your age is "+age+".";
 
 age=age+5;
 console.log("After 5 years, my age is "+age);
-document.getElementById("q1").innerHTML="Hello";
